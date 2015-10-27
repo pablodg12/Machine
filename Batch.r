58976a7c350241faa924267e11488336d2078885
pablodg12 <pabloibarra@pablos-MacBook-Pro.local>
2015-10-26 23:34:01 -0300

    Algunas modificaciones al stochastic, se agregan un par de funciones mas

--- Batch.r
+++ Batch.r
@@ -1,12 +1,12 @@
 #batch descendent algorithm#
 
-batch <- function(dataset, theta, alpha){
-	return(theta + alpha * colSums(cbind(1,dataset[1:12]) * (dataset[,13] - colSums(t(cbind(1,dataset[1:12]))*theta))))
+batch <- function(dataset, beta, alpha){
+
+	return (beta + alpha * t(cbind(1,as.matrix(dataset[1:12]))) %*% (dataset[,13] - (cbind(1,as.matrix(dataset[1:12])) %*% beta) ))
+
 }
 
-
-
-Lineal <- function(dataset, theta, alpha){
+stochastic  <- function(dataset, theta, alpha){
 	Data <- cbind(1,dataset[1:12]) 
 	for(i in 1:length(Data)[,1]){
 		theta <- theta + alpha * (Data[i,] *(dataset[,13] - sum(data[i,] *theta))
@@ -19,14 +19,14 @@ crossfolder <- function(dataset, partition){
 	for(i in 1:partition){
 		filename = paste("cross", i, ".data" , sep="")
 		write.table(dataset[ last : (length(dataset[,1]) * (i/partition)), 1:13], filename, row.names = FALSE, col.names = FALSE)
-		last = (length(dataset) * (i/partition))
+		last <- (length(dataset[,1]) * (i/partition) + 1)
 	}
 }
 
  prediction <- function(data, beta){
- 	print(colSums(t(cbind(1,data[1:12])) * beta))
- 	return((data[,13] - colSums(t(cbind(1,data[1:12])) * beta))^2)
+ 	return(cbind(1,as.matrix(data[1:12])) %*% beta )
  }
+
 
 standardization <- function(dataset){
 	data <- dataset
@@ -41,7 +41,6 @@ standardization <- function(dataset){
 
 normalization <-function(dataset){
 	data <- dataset
-
 	for(i in 1:length(dataset[,1])) {
 		for(k in 1:length(dataset[1,])) { 
 			data[i,k] <- ( dataset[i,k] - mean(dataset[,k]) ) / ( sd(dataset[,k]))
@@ -49,5 +48,9 @@ normalization <-function(dataset){
 	}
 	
 	return(data)
-
 }
+
+
+
+
+

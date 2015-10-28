87d89070e599d79228c426fc51c63676d896ed02
pablodg12 <pabloibarra@pablos-MacBook-Pro.local>
2015-10-28 16:10:50 -0300

    Se agrego la funcion para acalcular locally weighted linear regression, se modularizo aparte se creo la funcion para calcular los pesos

--- Algoritmos.r
+++ Algoritmos.r
@@ -1,19 +1,18 @@
-#batch descendent algorithm#
+######### Algoritmos Pregunta 1 Tarea #################
 
-batch <- function(dataset, beta, alpha){
+Batch <- function(dataset, beta, alpha){
 	delta <- t(cbind(1,as.matrix(dataset[1:12]))) %*% (dataset[,13] - (cbind(1,as.matrix(dataset[1:12])) %*% beta)) 
 	return(beta - alpha * delta)
 }
 
-stochastic  <- function(dataset, theta, alpha){
+Stochastic  <- function(dataset, theta, alpha){
 	Data <- cbind(1,dataset[1:12]) 
 	for(i in 1:length(Data[1,])) {
 		theta <- theta + alpha * ( Data[i,] * (dataset[i,13] - sum(Data[i,] * theta) ))
 	}
 	return(theta)
 }
-
-ascent <- function(dataset, beta, alpha){
+Ascent <- function(dataset, beta, alpha){
 	Data <- cbind(1,dataset[1:12]) 
 	for(i in 1:length(Data[1,])) {
 		theta <- theta + alpha * ( Data[i,] * (dataset[i,13] - 1/(1 + e ^(-sum(Data[i,] * theta))) ))
@@ -21,7 +20,18 @@ ascent <- function(dataset, beta, alpha){
 	return(theta)
 }
 
+Locally <- function(dataset, bandwidth){ 
+	return(((solve((t(cbind(1,as.matrix(dataset[1:12]))) %*% weight(dataset, bandwidth )) %*% cbind(1,as.matrix(dataset[1:12]))) %*% t(cbind(1,as.matrix(dataset[1:12])))) %*% weight(dataset, bandwidth)) %*% dataset[,13])
+}
 
+weight <- function(dataset, bandwidth){ ### Calcula los pesos
+	Data <- cbind(1,dataset[1:12])
+	weight <- c()
+	for(i in 1:length( Data[,1] )) {
+		weight[i] <-  exp ((  -1 * (  sum((Data[i,] - dataset[,13])  * (Data[i,] - dataset[,13]) ) / 2 * bandwidth^2 ) ))
+	}
+	return(diag(weight, length(weight), length(weight)))	
+}
 
 
 

3fc3aa99eb8ff57fa56f0865328dd9b31e903d74
pablodg12 <pabloibarra@pablos-MacBook-Pro.local>
2015-10-31 13:21:33 -0300

    Funcion que procesa data, y algunos datos no procesados completamente

--- ManejoDeDatos.r
+++ ManejoDeDatos.r
@@ -3,7 +3,17 @@ standardization <- function(dataset){
 
 	for(i in 1:length(dataset[,1])) {
 		for(k in 1:length(dataset[1,])) { 
-			data[i,k] <- 2 * ( dataset[i,k] - min(dataset[,k]) ) / ( max(dataset[,k]) - min(dataset[,k]) ) - 2
+			data[i,k] <- ( dataset[i,k] - min(dataset[,k]) ) / ( max(dataset[,k]) - min(dataset[,k]) )
+		}
+	}
+	return(data)
+}
+
+Specialstandardization <- function(dataset, dataset2){
+	data <- dataset2
+	for(i in 1:length(dataset[,1])) {
+		for(k in 1:length(dataset[1,])) { 
+			data[i,k] <- ( data[i,k] - min(dataset[,k]) ) / ( max(dataset[,k]) - min(dataset[,k]) ) 
 		}
 	}
 	return(data)
@@ -38,7 +48,7 @@ TrainBatch <- function(data, beta, alpha){
 	betab <- c(5,5,5,5,5,5,5,5,5,5,5,5,5)
 	repeat{
 		beta <- Batch(data, beta, alpha)
-		if(abs(sum(beta) - sum(betab)) < 0.005){
+		if( (abs( sum(betab) - sum(beta)) < 0.001) ) {
 			break
 		}
 		betab <- beta
@@ -50,18 +60,10 @@ TrainStocha <- function(data, beta, alpha){
 	betab <- c(5,5,5,5,5,5,5,5,5,5,5,5,5)
 	repeat{
 		beta <- Stochastic(data, beta, alpha)
-		if(abs(sum(beta) - sum(betab)) < 0.005){
+		if( (abs( sum(t(betab)) - sum(t(beta)) )  < 0.01) ) {
 			break
 		}
 		betab <- beta
-	}
-	return(beta)
-}
-
-
-TrainBatch <- function(data, beta, iterations, alpha){
-	for(i in 1:iterations){
-		beta <- Stochastic(data, beta, alpha)	
 	}
 	return(beta)
 }
--- RecolectarDatos.R
+++ RecolectarDatos.R
@@ -1,24 +1,44 @@
-GetData <- function(dataset){
+library(matrixStats)
 
-    alphaBatch <- c(10^(-8), 3*10^(-8), 6* 10^(-8), 9*10^(-8), 10^(-7))
-    alphaStoch <- c(2*10^(-6), 4*10^(-6), 6*10^(-6), 8*10^(-6), 10^(-5))
-    beta <- c(0,0,0,0,0,0,0,0,0,0,0,0,0)
-    aErrorBatch <- c()
-    aErrorStocha <- c()
+GetData <- function(dataset, number){
+    alphaBatch <- c(10^(-8) , 3*10^(-8), 6* 10^(-8) , 9*10^(-8)  , 10^(-7))
+    #alphaBatch <- c(10^(-4), 3*10^(-4), 6* 10^(-4), 9*10^(-4), 10^(-5))
+    #alphaStoch <- c(2*10^(-6), 4*10^(-6), 6*10^(-6), 8*10^(-6), 10^(-5))
+    #bandwidth <- c(10^(-3), 4*10^(-3), 6*10^(-3), 8*10^(-3), 10^(-4))
+
+    aErrorBatch <- matrix(1:1, nrow = 5, ncol = 5)
+    #aErrorStocha <- matrix(1:1, nrow = 5, ncol = 5)
 
     for(i in 1:5){
-    	Train <- dataset[1:(0.75*length(dataset)), 1:13]
-    	Test <- dataset[(0.75*length(dataset) + 1):length(dataset), 1:13]
-    	for(k in 1:5){
-	        betaBatch <-TrainBatch(Train, beta, alphaBatch[k] )
-	        betaStocha <-TrainStocha(Train, beta, alphaStoch[k] )
-	        aErrorBatch <- rbind(aErrorBatch, Error(Test, betaBatch) )
-	        aErrorStocha <- rbind(aErrorStocha, Error(Test, betaStocha) ) 
-    	}
-    	dataset <- rbind(Test, Train)	
+        Train <- dataset[1:(0.8*length(dataset[,1])), ]
+        Test <- dataset[ ((0.8*length(dataset[,1])) + 1): length(dataset[,1]) , ]
+        for(k in 1:5){
+            beta <- c(0,0,0,0,0,0,0,0,0,0,0,0,0)
+            betaBatch <-TrainBatch(Train, beta, alphaBatch[k])
+            #betaLocal <- Locally(Train, bandwidth[k])
+            #betaStocha <-TrainStocha(Train, beta, alphaStoch[k])
+            #betaNewton <-NewtonRaphson(Train, beta)
+            #aErrorStocha[i,k] = mean(Error(Test, betaLocal))
+            aErrorBatch[i,k] <- mean(Error(Test, betaBatch))        
+        }
+        dataset <- rbind(Test, Train)   
     }
-
-    print(colMeans(aErrorStocha))
+    name = paste("Training", number, "Set", sep="")
+    #print(colMeans(aErrorStocha))
+    print(aErrorBatch)
+    print(colSds(aErrorBatch))
     print(colMeans(aErrorBatch))
+    colnames(aErrorBatch) <- c("10^(-8)" , "3*10^(-8)", "6* 10^(-8)" , "9*10^(-8)"  , "10^(-7)")
+    boxplot(aErrorBatch, main = name) 
+    return(colSds(aErrorBatch)/colMeans(aErrorBatch))
 
+}
+
+
+General <-function(){
+    for(i in 1:20){
+        name = paste("Training", i, "cereales.data", sep="")
+        data <- read.table(name)
+        print(GetData(data, i))
+    }
 }
